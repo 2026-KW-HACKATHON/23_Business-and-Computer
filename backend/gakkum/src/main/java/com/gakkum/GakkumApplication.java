e4c6cd14eca1cdd52a0d4cef1f0abcc5a332dde5
holychan <east0321@outlook.kr>
2026-09-17 00:08:52 +0900

    chore: Backend 초기화

---
+++ backend/gakkum/src/main/java/com/gakkum/GakkumApplication.java
@@ -0,0 +1,13 @@
+package com.gakkum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakkumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GakkumApplication.class, args);
+    }
+
+}
