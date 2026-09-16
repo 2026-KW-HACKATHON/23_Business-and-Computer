1301e9edc12366570fea5cba6af2686138714b52
holychan <east0321@outlook.kr>
2026-09-17 00:12:37 +0900

    chore: Backend 초기화

---
+++ backend/src/test/java/com/gakkum/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gakkum.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
