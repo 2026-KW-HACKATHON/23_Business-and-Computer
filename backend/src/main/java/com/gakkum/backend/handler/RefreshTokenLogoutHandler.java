2d7dced2640850d56bb6995a3803f7d61c64d6c7
holychan <east0321@outlook.kr>
2026-09-20 22:16:15 +0900

    chore: 소셜 로그인 관련 핸들러 및 리프레시 토큰 핸들러 구현

--- backend/src/main/java/com/gakkum/backend/handler/RefreshTokenLogoutHandler.java
+++ backend/src/main/java/com/gakkum/backend/handler/RefreshTokenLogoutHandler.java
@@ -1,4 +1,57 @@
 package com.gakkum.backend.handler;
 
-public class RefreshTokenLogoutHandler {
+import com.gakkum.backend.domain.jwt.service.JwtService;
+import com.gakkum.backend.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.util.StringUtils;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RefreshTokenLogoutHandler implements LogoutHandler {
+
+    private final JwtService jwtService;
+    private final JWTUtil jwtUtil;
+
+    public RefreshTokenLogoutHandler(JwtService jwtService, JWTUtil jwtUtil) {
+        this.jwtService = jwtService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        try {
+            String body = new BufferedReader(new InputStreamReader(request.getInputStream()))
+                    .lines().reduce("", String::concat);
+
+            if (!StringUtils.hasText(body)) return;
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(body);
+            String refreshToken = jsonNode.has("refreshToken") ? jsonNode.get("refreshToken").asText() : null;
+
+            // 유효성 검증
+            if (refreshToken == null) {
+                return;
+            }
+            Boolean isValid = jwtUtil.isValid(refreshToken, false);
+            if (!isValid) {
+                return;
+            }
+
+            // Refresh 토큰 삭제
+            jwtService.removeRefresh(refreshToken);
+
+        } catch (
+                IOException e) {
+            throw new RuntimeException("Failed to read refresh token", e);
+        }
+    }
+
 }
--- backend/src/main/java/com/gakkum/backend/handler/SocialSuccessHandler.java
+++ backend/src/main/java/com/gakkum/backend/handler/SocialSuccessHandler.java
@@ -1,4 +1,53 @@
 package com.gakkum.backend.handler;
 
-public class SocialSuccessHandler {
+import com.gakkum.backend.domain.jwt.service.JwtService;
+import com.gakkum.backend.util.JWTUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Qualifier("SocialSuccessHandler")
+@RequiredArgsConstructor
+public class SocialSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        // username, role
+        String username =  authentication.getName();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+
+        // JWT(Refresh) 발급
+        String refreshToken = jwtUtil.createJWT(username, "ROLE_" + role, false);
+
+        // 발급한 Refresh DB 테이블 저장 (Refresh whitelist)
+        jwtService.addRefresh(username, refreshToken);
+
+        // 응답
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+                .path("/")
+                .sameSite("Lax")
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(60)
+                .domain("api.hubspacekw.com")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        response.sendRedirect("http://localhost:5173/cookie");
+    }
+
 }
