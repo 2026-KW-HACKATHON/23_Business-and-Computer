ca473ba2f1439bb3259e3306a811af15ceebe91e
holychan <east0321@outlook.kr>
2026-09-20 22:14:44 +0900

    chore: OAuth2 관련 SecurityConfig 설정 추가

--- backend/src/main/java/com/gakkum/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/gakkum/backend/config/SecurityConfig.java
@@ -2,12 +2,21 @@ package com.gakkum.backend.config;
 
 import java.util.List;
 
+import com.gakkum.backend.domain.jwt.service.JwtService;
+import com.gakkum.backend.domain.user.service.UserService;
+import com.gakkum.backend.filter.JWTFilter;
+import com.gakkum.backend.handler.RefreshTokenLogoutHandler;
+import com.gakkum.backend.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,18 +29,52 @@ public class SecurityConfig {
 
 
     private final RestAuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationSuccessHandler socialSuccessHandler;
+    private final JwtService jwtService;
+    private final JWTFilter jwtFilter;
+    private final JWTUtil jwtUtil;
+    private final UserService userService;
 
     /**
      * 401 응답을 우리 형식에 맞춰서 주기위한 메서드
      */
-    public SecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint) {
+    public SecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint,
+                          @Qualifier("SocialSuccessHandler") AuthenticationSuccessHandler socialSuccessHandler,
+                          JwtService jwtService,
+                          JWTFilter jwtFilter,
+                          UserService userService,
+                          JWTUtil jwtUtil) {
         this.authenticationEntryPoint = authenticationEntryPoint;
+        this.socialSuccessHandler = socialSuccessHandler;
+        this.jwtService = jwtService;
+        this.jwtFilter = jwtFilter;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(userService))
+                        .successHandler(socialSuccessHandler));
+
+        http
+                .logout(logout -> logout.addLogoutHandler(new RefreshTokenLogoutHandler(jwtService, jwtUtil)));
+
+        http
+                .addFilterBefore(jwtFilter, LogoutFilter.class);
+
+        http
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
