85181a741fd824d5127f28946a115af117736132
holychan <east0321@outlook.kr>
2026-09-21 16:49:05 +0900

    chore: common 디렉토리명 global 로 변경

--- backend/src/main/java/com/gakkum/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/gakkum/backend/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.gakkum.backend.common.exception;
+package com.gakkum.backend.global.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
-import com.gakkum.backend.common.response.ApiResponse;
+import com.gakkum.backend.global.response.ApiResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
